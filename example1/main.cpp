1dc03a3943ee39e1defa8f963806c0ce05af2f8d
Jordi Villar <jrdi.villar@gmail.com>
2012-12-28 14:56:24 +0100

    example1: code improvements

--- example1/main.cpp
+++ example1/main.cpp
@@ -1,20 +1,23 @@
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+#include "cv.h"
+#include "highgui.h"
 
 int main( int argc, char** argv )
 {
-  cv::Mat image, gray_image;
-  image = cv::imread( argv[1], 1 );
+  if( argc != 2 ) {
+    std::cerr << "Usage: " << argv[0] << " <InputImage>" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  if( argc != 2 || !image.data ) {
-    printf( "No image data \n" );
-    return -1;
+  cv::Mat image = cv::imread( argv[1], 1 );
+  if(!image.data) {
+    return EXIT_FAILURE;
   }
 
   cv::namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
-  cv::imshow( "Display Image", gray_image );
-
+  cv::imshow( "Display Image", image );
   cv::waitKey(0);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
