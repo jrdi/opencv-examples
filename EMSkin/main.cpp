6ec1d7c6d11ed59de4f9b28f67e08a73d90bc4b7
Jordi Villar <jordi.villar@crisalix.com>
2013-07-02 00:47:34 +0200

    add grid

--- EMSkin/main.cpp
+++ EMSkin/main.cpp
@@ -5,6 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int patchX = 25; int patchY = 25;
+
+cv::Rect patchGridRect(int idX, int idY) {
+  return cv::Rect(idX * patchX, idY * patchY,
+                  patchX, patchY);
+}
+
+void addPatchGrid(const cv::Mat& img, std::vector<cv::Mat>& samples, 
+                  int idX, int idY)
+{
+  cv::Rect patch = patchGridRect(idX, idY);
+
+  samples.push_back(cv::Mat(img, patch).clone());
+}
+
 int main( int argc, char** argv ) {
 
   if( argc != 3 ) {
@@ -22,84 +37,72 @@ int main( int argc, char** argv ) {
     return EXIT_FAILURE;
   }
 
+  std::vector<cv::Mat> skinImages;
+  addPatchGrid(image, skinImages, 5, 6);
+  addPatchGrid(image, skinImages, 6, 7);
+  addPatchGrid(image, skinImages, 7, 8);
+
+  // cv::rectangle(image, patchGridRect(12, 10), cv::Scalar(0, 0, 255));
+  // cv::rectangle(image, patchGridRect(12, 11), cv::Scalar(0, 0, 255));
+  // cv::rectangle(image, patchGridRect(12, 12), cv::Scalar(0, 0, 255));
+  // cv::rectangle(image, patchGridRect(12, 13), cv::Scalar(0, 0, 255));
+  // cv::rectangle(image, patchGridRect(12, 14), cv::Scalar(0, 0, 255));
+  // cv::rectangle(image, patchGridRect(12, 15), cv::Scalar(0, 0, 255));
+
   cv::Mat CbCrImage;
   cv::cvtColor( image, CbCrImage, CV_BGR2YCrCb );
-  cv::cvtColor( trainImage, trainImage, CV_BGR2YCrCb );
-
   CbCrImage.convertTo(CbCrImage, CV_64F);
-  trainImage.convertTo(trainImage, CV_64F);
 
-  cv::Mat trainSamples = cv::Mat(trainImage.rows*trainImage.cols, 2, CV_64F);
+  cv::Mat trainSamples = cv::Mat(patchX*patchY*skinImages.size(), 2, CV_64FC1);
+  for(size_t i = 0; i < skinImages.size(); ++i) {
+    cv::cvtColor( skinImages[i], skinImages[i], CV_BGR2YCrCb );
+    skinImages[i].convertTo(skinImages[i], CV_64FC3);
 
-  for( int j = 0; j < trainImage.rows ; j++ ) { 
-    for( int i = 0; i < trainImage.cols; i++ ) {
-      cv::Vec3d color = trainImage.at<cv::Vec3d>(j, i);
+    for( int y = 0; y < skinImages[0].rows ; y++ ) { 
+      for( int x = 0; x < skinImages[0].cols; x++ ) {
+        cv::Vec3d color = skinImages[0].at<cv::Vec3d>(y, x);
 
-      trainSamples.at<double>(j*i, 0) = color[1];
-      trainSamples.at<double>(j*i, 1) = color[2];
+        trainSamples.at<double>(y*x+i*patchX*patchY, 0) = color[1];
+        trainSamples.at<double>(y*x+i*patchX*patchY, 1) = color[2];
+      }
     }
   }
 
-  int nClusters = 2;
-  cv::Mat means(2, 2, CV_64FC1);  
-  means.at<double>(0, 0) = 0;
-  means.at<double>(0, 1) = 0;
-  means.at<double>(1, 0) = 153.;
-  means.at<double>(1, 1) = 100.;
+  cv::Mat skinSample(1, 2, CV_64F);
+  cv::Mat skinMask(CbCrImage.size(), CV_64FC1);
 
-  cv::Mat sample(1, 2, CV_64F);
-  cv::Mat mask1(CbCrImage.size(), CV_64FC1);
-  cv::Mat mask2(CbCrImage.size(), CV_32FC1);
-
-  cv::EM em_skin = cv::EM(nClusters);
-  if(em_skin.trainE(trainSamples, means)) {
+  cv::EM emSkin = cv::EM(2);
+  if(emSkin.train(trainSamples)) {
     for( int j = 0; j < CbCrImage.rows ; j++ ) { 
       for( int i = 0; i < CbCrImage.cols; i++ ) {
         cv::Vec3d color = CbCrImage.at<cv::Vec3d>(j, i);
 
-        sample.at<double>(0) = color[1];
-        sample.at<double>(1) = color[2];
+        skinSample.at<double>(0) = color[1];
+        skinSample.at<double>(1) = color[2];
 
-        cv::Vec2d prob = em_skin.predict(sample);
-        mask1.at<double>(j, i, 0) = prob[0];
-        mask2.at<double>(j, i, 0) = prob[1];
+        cv::Vec2d prob = emSkin.predict(skinSample);
+        skinMask.at<double>(j, i, 0) = prob[0];
       }
     }
   } else {
-    std::cerr << "Error!" << std::endl;
+    std::cerr << "Failed training Skin EM" << std::endl;
   }
 
-  std::cout << "Initial params: " << std::endl;
-  std::cout << "Clusters: " << nClusters << std::endl;
-  std::cout << "Means: " << means << std::endl;
-  std::cout << "Trained params: " << std::endl;
-  std::cout << "Weights: " << em_skin.get<cv::Mat>("weights") << std::endl;
-  std::cout << "Means: " << em_skin.get<cv::Mat>("means") << std::endl;
-  std::cout << "Covs: " << std::endl;
-  std::vector<cv::Mat> covs = em_skin.get<std::vector<cv::Mat> >("covs");
-  for (int i = 0; i < nClusters; ++i) {
-    std::cout << covs[i] << std::endl;
-  }
-  std::cout << "Iterations: " << em_skin.get<int>("maxIters") << std::endl;
+  skinMask.convertTo(skinMask, CV_32FC1);
 
-  double min, max;
-  cv::minMaxLoc(mask1, &min, &max);
-  std::cout << min << " " << max << std::endl;
+  cv::normalize(skinMask, skinMask, 0., 255., cv::NORM_MINMAX);
+  cv::imwrite("mask1.jpg", skinMask);
 
-  mask1.convertTo(mask1, CV_32FC1);
-  cv::threshold(mask1, mask2, -40., 255., cv::THRESH_BINARY);
-
-  cv::normalize(mask1, mask1, 0., 255., cv::NORM_MINMAX);
-  cv::imwrite("mask1.jpg", mask1);
+  cv::threshold(skinMask, skinMask, 230., 255., cv::THRESH_BINARY);
 
   int erosion_size = 3;
   cv::Mat element = getStructuringElement(cv::MORPH_ELLIPSE,
                                           cv::Size( 2 * erosion_size + 1, 2 * erosion_size + 1 ));
 
-  cv::dilate( mask2, mask2, element );
-  cv::erode( mask2, mask2, element );
+  cv::dilate( skinMask, skinMask, element );
+  cv::erode( skinMask, skinMask, element );
 
-  cv::imwrite("mask2.jpg", mask2);
+  cv::imwrite("mask2.jpg", skinMask);
 
   return EXIT_SUCCESS;
 }
