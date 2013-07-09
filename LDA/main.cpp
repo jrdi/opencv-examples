03ec57e8a079d8a119e7fa31f43bd399c8e2044e
Jordi Villar <jordi.villar@crisalix.com>
2013-07-09 15:55:45 +0200

    use RGB+HSV

--- LDA/main.cpp
+++ LDA/main.cpp
@@ -7,15 +7,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 cv::Mat lda(cv::Mat & X, std::vector<int> y) {
   int dimensions = X.cols;
-  cv::Mat Sw = cv::Mat::zeros(dimensions, dimensions, CV_32F);
-  cv::Mat Sb = cv::Mat::zeros(dimensions, dimensions, CV_32F);
+  cv::Mat Sw = cv::Mat::zeros(dimensions, dimensions, CV_64F);
+  cv::Mat Sb = cv::Mat::zeros(dimensions, dimensions, CV_64F);
 
   cv::Mat Covar, Mu;
-  cv::calcCovarMatrix(X, Covar, Mu, CV_COVAR_NORMAL+CV_COVAR_ROWS, CV_32F);
-
-  std::cout << "Mu: " << Mu << std::endl;
+  cv::calcCovarMatrix(X, Covar, Mu, CV_COVAR_NORMAL+CV_COVAR_ROWS, CV_64F);
 
   cv::Mat X0, X1;
   for (int i = 0; i < y.size(); ++i)
@@ -31,51 +30,48 @@ cv::Mat lda(cv::Mat & X, std::vector<int> y) {
   int n1 = X1.rows;
 
   cv::Mat Covar0, Covar1, Mu0, Mu1;
-  cv::calcCovarMatrix(X0, Covar0, Mu0, CV_COVAR_NORMAL+CV_COVAR_ROWS, CV_32F);
-  cv::calcCovarMatrix(X1, Covar1, Mu1, CV_COVAR_NORMAL+CV_COVAR_ROWS, CV_32F);
-
-  std::cout << "Bg Mu: " << Mu0 << std::endl;
-  std::cout << "Skin Mu: " << Mu1 << std::endl;
+  cv::calcCovarMatrix(X0, Covar0, Mu0, CV_COVAR_NORMAL+CV_COVAR_ROWS, CV_64F);
+  cv::calcCovarMatrix(X1, Covar1, Mu1, CV_COVAR_NORMAL+CV_COVAR_ROWS, CV_64F);
 
   cv::Mat XM0(X0.size(), X0.type()), XM1(X1.size(), X1.type());
   for(int i = 0; i < X0.rows; i++) {
     for(int j = 0; j < dimensions; j++) {
-      XM0.at<float>(i, j) = X0.at<float>(i, j) - Mu0.at<float>(j);
+      XM0.at<double>(i, j) = X0.at<double>(i, j) - Mu0.at<double>(j);
     }
   }
 
   for(int i = 0; i < X1.rows; i++) {
     for(int j = 0; j < dimensions; j++) {
-      XM1.at<float>(i, j) = X1.at<float>(i, j) - Mu1.at<float>(j);
+      XM1.at<double>(i, j) = X1.at<double>(i, j) - Mu1.at<double>(j);
     }
   }
 
   Sw = (XM0.t() * XM0) + (XM1.t() * XM1);
-  std::cout << "Sw: " << Sw << std::endl;
   
-  cv::Mat M0M(dimensions, 1, CV_32F);
+  cv::Mat M0M(dimensions, 1, CV_64F);
   for(int j = 0; j < dimensions; j++) {
-    M0M.at<float>(j) = Mu0.at<float>(j) - Mu.at<float>(j);
+    M0M.at<double>(j) = Mu0.at<double>(j) - Mu.at<double>(j);
   }
 
-  cv::Mat M1M(dimensions, 1, CV_32F);
+  cv::Mat M1M(dimensions, 1, CV_64F);
   for(int j = 0; j < dimensions; j++) {
-    M1M.at<float>(j) = Mu1.at<float>(j) - Mu.at<float>(j);
+    M1M.at<double>(j) = Mu1.at<double>(j) - Mu.at<double>(j);
   }
 
   Sb = n0 * M0M * M0M.t() + n1 * M1M * M1M.t();
-  std::cout << "Sb: " << Sb << std::endl;
 
   cv::Mat S = Sw.inv(cv::DECOMP_SVD) * Sb;
-  std::cout << "S: " << S << std::endl;
 
   cv::Mat eigenvalues(1, dimensions, Sb.type()), eigenvectors(dimensions, dimensions, Sb.type());
   cv::eigen(S, eigenvalues, eigenvectors);
 
-  std::cout << eigenvalues << std::endl;
-  std::cout << eigenvectors << std::endl;
+  cv::Mat W;
 
-  return eigenvectors;
+  W.push_back(eigenvectors.row(0));
+  W.push_back(eigenvectors.row(1));
+  W.push_back(eigenvectors.row(2));
+
+  return W;
 }
 
 int main( int argc, char** argv )
@@ -98,23 +94,34 @@ int main( int argc, char** argv )
     return EXIT_FAILURE;
   }
 
-  cv::Mat samples(bgImage.cols*bgImage.rows+skinImage.cols*skinImage.rows, 2, CV_32F);
+  cv::Mat samples(bgImage.cols*bgImage.rows+skinImage.cols*skinImage.rows, 6, CV_64F);
   std::vector<int> labels(bgImage.cols*bgImage.rows+skinImage.cols*skinImage.rows);
 
-  bgImage.convertTo(bgImage, CV_32F);
-  skinImage.convertTo(skinImage, CV_32F);
+  cv::Mat bgHSVImage, skinHSVImage;
 
-  cv::cvtColor( bgImage, bgImage, CV_BGR2YCrCb );
-  cv::cvtColor( skinImage, skinImage, CV_BGR2YCrCb );
+  cv::cvtColor( bgImage, bgHSVImage, CV_BGR2HSV );
+  cv::cvtColor( skinImage, skinHSVImage, CV_BGR2HSV );
+
+  bgImage.convertTo(bgImage, CV_64F);
+  skinImage.convertTo(skinImage, CV_64F);
+  bgHSVImage.convertTo(bgHSVImage, CV_64F);
+  skinHSVImage.convertTo(skinHSVImage, CV_64F);
+
 
   int count = 0;
 
   for( int y = 0; y < bgImage.rows ; y++ ) { 
     for( int x = 0; x < bgImage.cols; x++ ) {
-      cv::Vec3f color = bgImage.at<cv::Vec3f>(y, x);
+      cv::Vec3d color = bgImage.at<cv::Vec3d>(y, x);
+      cv::Vec3d colorHSV = bgHSVImage.at<cv::Vec3d>(y, x);
 
-      samples.at<float>(count, 0) = color[1];
-      samples.at<float>(count, 1) = color[2];
+      samples.at<double>(count, 0) = color[0];
+      samples.at<double>(count, 1) = color[1];
+      samples.at<double>(count, 2) = color[2];
+
+      samples.at<double>(count, 3) = colorHSV[0];
+      samples.at<double>(count, 4) = colorHSV[1];
+      samples.at<double>(count, 5) = colorHSV[2];
 
       labels[count] = 0;
 
@@ -124,10 +131,16 @@ int main( int argc, char** argv )
 
   for( int y = 0; y < skinImage.rows ; y++ ) { 
     for( int x = 0; x < skinImage.cols; x++ ) {
-      cv::Vec3f color = skinImage.at<cv::Vec3f>(y, x);
+      cv::Vec3d color = skinImage.at<cv::Vec3d>(y, x);
+      cv::Vec3d colorHSV = skinHSVImage.at<cv::Vec3d>(y, x);
 
-      samples.at<float>(count, 0) = color[1];
-      samples.at<float>(count, 1) = color[2];
+      samples.at<double>(count, 0) = color[0];
+      samples.at<double>(count, 1) = color[1];
+      samples.at<double>(count, 2) = color[2];
+
+      samples.at<double>(count, 3) = colorHSV[0];
+      samples.at<double>(count, 4) = colorHSV[1];
+      samples.at<double>(count, 5) = colorHSV[2];
 
       labels[count] = 1;
 
@@ -135,32 +148,48 @@ int main( int argc, char** argv )
     }
   }
 
-  std::ofstream ofs;
-  ofs.open( "myFile.txt" );
+  std::ofstream ofs, ofs2;
+  ofs.open( "X.txt" );
+  ofs2.open( "y.txt" );
 
   for( int i = 0; i < samples.rows; i++) {
-    ofs << samples.at<float>(i, 0) << " " << samples.at<float>(i, 1) << std::endl;
+    ofs << samples.at<double>(i, 0) << " " << samples.at<double>(i, 1) << " ";
+    ofs << samples.at<double>(i, 2) << " " << samples.at<double>(i, 3) << " ";
+    ofs << samples.at<double>(i, 4) << " " << samples.at<double>(i, 5) << std::endl;
+
+    ofs2 << labels[i] << std::endl;
   }
 
-  ofs.close();
+  ofs.close(); ofs2.close();
 
-  cv::Mat convMat = lda(samples, labels);
+  cv::Mat W = lda(samples, labels);
 
-  image.convertTo(image, CV_32F);
-  cv::cvtColor( image, image, CV_BGR2YCrCb );
+  cv::Mat imageHSV;
+
+  cv::cvtColor(image, imageHSV, CV_BGR2HSV);
+  image.convertTo(image, CV_64F);
+  imageHSV.convertTo(imageHSV, CV_64F);
   cv::imwrite("cbcr.jpg", image);
 
   count = 0;
   for( int y = 0; y < image.rows ; y++ ) { 
     for( int x = 0; x < image.cols; x++ ) {
-      cv::Vec3f color = image.at<cv::Vec3f>(y, x);
-      cv::Mat sample(2, 1, CV_32F), resample(2, 1, CV_32F);
+      cv::Vec3d color = image.at<cv::Vec3d>(y, x);
+      cv::Vec3d colorHSV = imageHSV.at<cv::Vec3d>(y, x);
 
-      sample.at<float>(0) = color[1];
-      sample.at<float>(1) = color[2];
+      cv::Mat sample(6, 1, CV_64F), resample(3, 1, CV_64F);
 
-      resample = convMat * sample;
-      image.at<cv::Vec3f>(y, x) = cv::Vec3f(color[0], resample.at<float>(0), resample.at<float>(1));
+      sample.at<double>(0) = color[0];
+      sample.at<double>(1) = color[1];
+      sample.at<double>(2) = color[2];
+
+      sample.at<double>(3) = colorHSV[0];
+      sample.at<double>(4) = colorHSV[1];
+      sample.at<double>(5) = colorHSV[2];
+
+      resample = W * sample;
+
+      image.at<cv::Vec3d>(y, x) = cv::Vec3d(resample.at<double>(0), resample.at<double>(1), resample.at<double>(2));
 
       count++;
     }
